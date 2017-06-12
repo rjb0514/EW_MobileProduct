5b04d794478ac384d4c3c2c0c779ca0cd78ab0c7
ru <ru@.com>
2017-06-12 10:19:10 +0800

    CreatProduct

---
+++ EW_Mobile/EW_Mobile/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EW_Mobile
+//
+//  Created by ru on 2017/6/12.
+//  Copyright © 2017年 GMJK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
